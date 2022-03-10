79b8302690d149d55984ca558601115094a6ba59
NicolasMeseguer <nicolasmeseguer@icloud.com>
2022-03-10 22:22:03 +0100

    Particion aleatoria de los vectores

--- pygcn/layers.py
+++ pygcn/layers.py
@@ -5,6 +5,8 @@ import torch
 from torch.nn.parameter import Parameter
 from torch.nn.modules.module import Module
 
+from random import randrange
+import numpy as np
 
 class GraphConvolution(Module):
     """
@@ -33,9 +35,13 @@ class GraphConvolution(Module):
         if self.bias is not None:
             self.bias.data.uniform_(-stdv, stdv)
 
+    # Simulates METIS. Subgraphs are not balanced, one may have more load than another
     def random_partition(self, nvectors, nparts):
+        partitions = [[] for x in range(nparts)]
+        for i in range(nvectors):
+            partitions[randrange(nparts)].append(i)
 
-        return nvectors
+        return partitions
 
     def forward(self, input, adj):
         # Step no. 6 (forwarding of the layers)
@@ -44,7 +50,9 @@ class GraphConvolution(Module):
         support = torch.mm(input, self.weight)
 
         # randomly partition the graph into self.nparts
-        subgraphs = self.random_partition(list(adj.shape)[0], self.nparts)
+        subgraphs = self.random_partition(int(adj.shape[0]), self.nparts)
+
+        # edge blocks, calculate adj matrix for each and process it (look algorith PCGCN)
 
         #agregacion
         output = torch.spmm(adj, support)
